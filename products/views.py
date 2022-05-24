aa926b4e70e03344fd10d620ed75e7773cf05e66
rupert-ma <michael.a.rupert@gmail.com>
2022-05-24 09:58:15 -0700

    created products model + product serializer + rest framework in settings + updated project urls + created first view function

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def product_list(request):
+    return Response('ok')
+
+@api_view(['GET'])
+def product_detail(request):
+    return Response('o OK')
